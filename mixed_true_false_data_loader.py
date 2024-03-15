9eba47403e9a8fb4ed782b60aa6ebe16a4256cb8
PascualMeritaTorres <pmeritatorres@gmail.com>
2024-03-15 12:46:38 +0000

    Add layer param. for batched embeddings

--- mixed_true_false_data_loader.py
+++ mixed_true_false_data_loader.py
@@ -4,6 +4,8 @@ from pathlib import Path
 import numpy as np
 import pandas as pd
 
+import os
+
 def get_file_word_count(file):
     return int(subprocess.check_output(['wc', file]).decode().split()[0])
 
@@ -20,30 +22,38 @@ def get_batch_of_statements(number=None, split='train', reindex=False):
     for csv_path in folder.glob('*.csv'):
         frame = pd.read_csv(csv_path)
         source = str(csv_path).split('/')[-1].split('.')[0]
-        frame['source'] = source
+        frame['source'] = source #source is the dataset name, so like 'anims_true_false' or like 'numbers'
         frames.append(frame[['statement', 'label', 'source']])
 
     df = pd.concat(frames)
     df = df.iloc[rng.permutation(len(df))]
 
     if number is not None:
-        df = df.iloc[:number]
+        df = df.iloc[:number] #get the first number of statements
 
     if reindex:
         df = df.set_index([df.index, 'source'])
 
     return df
 
-
-def get_batch_of_embeddings(number=None, split='train'):
+def get_batch_of_embeddings(number=None, split='train', layer=-1):
     rng = np.random.default_rng(17)
     frames = []
-    folder = Path('llama-true-false-results') / split
-    for csv_path in folder.glob('*.csv'):
+    folder = Path('../llama-true-false-results') / split #remove the ../ depending on where it is called from
+    if isinstance(layer, int):
+        pattern = f'*{str(layer)}.csv'
+    else:
+        pattern = '*.csv'
+
+    #print("Current working directory:", os.getcwd())
+    #print("Folder path:", folder)
+
+
+    for csv_path in folder.glob(pattern):
         frame = pd.read_csv(csv_path)
         source = str(csv_path).split('/')[-1].split('.')[0]
         frame['source'] = source
-        frames.append(frame[['statement', 'label', 'source']])
+        frames.append(frame[['statement', 'label', 'embeddings', 'source']])
 
     df = pd.concat(frames)
  
@@ -51,4 +61,4 @@ def get_batch_of_embeddings(number=None, split='train'):
         return df
 
     df = df.iloc[rng.permutation(len(df))]
-    return df.iloc[number]
+    return df.iloc[:number]

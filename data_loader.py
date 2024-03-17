3c30d956f0627434b9c96571524ea39763f74fee
tom <T.Bermejo@sms.ed.ac.uk>
2024-03-17 14:53:18 +0000

    Extend data loader to load QA data

--- data_loader.py
+++ data_loader.py
@@ -1,5 +1,6 @@
 import subprocess
 from pathlib import Path
+import re
 
 import numpy as np
 import pandas as pd
@@ -36,23 +37,39 @@ def get_batch_of_statements(number=None, split='train', reindex=False):
 
     return df
 
-def get_batch_of_embeddings(number=None, split='train', layer=-1):
+def get_batch_of_embeddings(number=None, source='tf', split='train', layer=-1, topic=None):
     rng = np.random.default_rng(17)
     frames = []
-    folder = Path(__file__).parent.resolve() / 'llama-true-false-results' / split
-    if layer is None:
-        pattern = '*.csv'
-    else:
-        pattern = f'*{layer}.csv'
-    #print("Current working directory:", os.getcwd())
-    #print("Folder path:", folder)
-    for csv_path in folder.glob(pattern):
-        frame = pd.read_csv(csv_path)
-        source = str(csv_path).split('/')[-1].split('.')[0]
-        frame['source'] = source
-        frame['layer'] = layer
-        frames.append(frame[['statement', 'label', 'source', 'layer', 'embeddings']])
+    data_folder = 'llama-qa-results' if source == 'qa' else 'llama-true-false-results'
+    path = Path(__file__).parent.resolve() / data_folder / split
 
+    layer_pat = str(layer) if layer is not None else r'[\-0-9]+'
+    if source == 'qa':
+        pattern = 'answers_' + layer_pat + '.csv'
+    else:
+        pattern = 'embeddings(.*)7B_' + layer_pat + '.csv'
+
+    pattern = re.compile(pattern)
+
+    data_files = []
+    for csv_path in path.glob('*.csv'):
+        filename_match = pattern.match(csv_path.name)
+        if filename_match is None:
+            continue
+
+        data_files.append(str(csv_path))
+        frame = pd.read_csv(csv_path)
+        if source == 'qa':
+            frame.rename({'answer_is_correct': 'label'}, axis='columns', inplace=True)
+            tpl = 'Q: {} A: {}'
+            frame['statement'] = frame.apply(lambda row: tpl.format(row.question, row.answer), axis=1)
+        topic = 'qa' if source == 'qa' else filename_match.groups()[0]
+        frame['topic'] = topic
+        frame['layer'] = int(layer)
+        frame['label'] = frame.label.astype(int)
+        frames.append(frame[['statement', 'label', 'topic', 'layer', 'embeddings']])
+
+    print('Loaded data from:\n\n', '\n'.join(data_files))
     df = pd.concat(frames)
     df = df.iloc[rng.permutation(len(df))]
  

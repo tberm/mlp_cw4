2fa26476e9468d653387372db7b9a626f1bb3d34
tom <T.Bermejo@sms.ed.ac.uk>
2024-03-18 20:13:16 +0000

    add mode to extract embeddings from generative answers

--- get_qa_probs_and_embeddings.py
+++ get_qa_probs_and_embeddings.py
@@ -47,7 +47,115 @@ def build_prompt(question, prompt_style):
     return prompt
 
 
-def main(model_name, questions_path, output_path, prompt_style, continue_partial=False, cpu_only=False):
+def run_gen(model_name, questions_path, output_path, prompt_style, continue_partial=False, cpu_only=False):
+    with open("config.json") as config_file:
+        config = json.load(config_file)
+
+    layers = [-1, -5, -9, -13]
+    #layers = [-1, -5]
+
+    if output_path.exists():
+        if not continue_partial:
+            raise FileExistsError(f'Output dir {output_path} already exists (use --continue-partial to add to results)')
+    else: 
+        if continue_partial:
+            raise FileNotFoundError(f'Passed --continue-partial but {output_path} does not exist')
+        output_path.mkdir()
+        with (output_path / 'MANIFEST').open('w') as manifest:
+            manifest.writelines([
+                f'model_name={model_name}\n',
+                f'prompt_style={prompt_style}\n',
+                f'layers={layers}\n',
+                f'started_at={datetime.utcnow()}\n',
+            ])
+
+    if 'llama' in model_name:
+        llama_model_path = Path(config['llama_model_path'])
+        model, tokenizer = load_llama_model(llama_model_path, cpu_only)
+    elif 'pythia' in model_name:
+        model, tokenizer = load_pythia_model(model_name)
+
+    device = 'cpu' if cpu_only else 'cuda:0'
+ 
+    questions = pd.read_csv(questions_path)
+
+    output_cols = [
+        'question', 'question_idx', 'answer', 'answer_is_correct', 'avg_log_prob', 'sum_log_prob',
+    ] + [
+        f'activations_layer_{layer_num}' for layer_num in layers
+    ]
+
+    output_path = output_path / 'answers.csv'
+
+    if not output_path.exists():
+        # create file with just the the header row
+        with output_path.open('w') as csv_file:
+            writer = csv.DictWriter(csv_file, output_cols)
+            writer.writeheader()
+        start_at_idx = 0
+    else:
+        # get last line in file
+        last_line = subprocess.check_output(['tail', '-1', str(output_path)])
+        match = re.match(r'[^,]*,(\d+)', last_line.decode())
+        if match is None:
+            start_at_idx = 0
+        else:
+            start_at_idx = int(match.groups()[0]) + 1
+
+    print(f'Starting at question {start_at_idx}')
+
+    model.eval()
+
+    pbar = tqdm(questions[start_at_idx:].iterrows(), total=len(questions) - start_at_idx)
+    for i, row in pbar:
+        question = row['Question']
+        answer = row['llama2-7b-chat']
+
+        pbar.set_description(question)
+
+        # row of results corresponding to this question
+        results = {}
+        results['question'] = question
+        results['question_idx'] = i
+
+        prompt = build_prompt(question, prompt_style)
+        # tokenise just so we know how long the prompt is
+        prompt_tokens = tokenizer(prompt, return_tensors="pt")
+        num_prompt_tokens = prompt_tokens['input_ids'].size(-1)
+        full_input = prompt + answer
+        inputs = tokenizer(full_input, return_tensors="pt").to(device)
+        # last token of prompt input gets replaced by first token of answer in full
+        # tokenised sequence. So answer starts at index num_prompt_tokens-1 and question
+        # ends at the token before that. 
+        assert inputs['input_ids'][0][num_prompt_tokens-2] == prompt_tokens['input_ids'][0][-2]
+
+        with torch.no_grad():
+            outputs = model(**inputs, output_hidden_states=True, return_dict=True) 
+
+        # get probability distribution over tokens at each token position
+        log_probs = outputs.logits[0].log_softmax(-1)  # (num_tokens, vocab_size)
+        # skip probabilities of question tokens
+        # the -1s are because to get the prob of token t we need to look at
+        # output at position t-1
+        log_probs = log_probs[num_prompt_tokens-2:-1, :]
+        # get only the probs of tokens in the answer
+        answer_tokens = inputs['input_ids'][0][num_prompt_tokens-1:]
+        answer_log_probs = log_probs[range(log_probs.shape[0]), answer_tokens]
+        results['avg_log_prob'] = answer_log_probs.mean().cpu().item()
+        results['sum_log_prob'] = answer_log_probs.sum().cpu().item()
+
+        # get activations
+        for layer in layers:
+            col_name = f'activations_layer_{layer}'
+            results[col_name] = outputs.hidden_states[layer][0][-1].cpu().numpy().tolist()
+
+        # write results
+        with output_path.open('a') as csv_file:
+            writer = csv.DictWriter(csv_file, output_cols)
+            writer.writerow(results)
+
+
+def run_mc(model_name, questions_path, output_path, prompt_style, continue_partial=False, cpu_only=False):
     with open("config.json") as config_file:
         config = json.load(config_file)
 
@@ -234,9 +342,13 @@ if __name__ == '__main__':
         help='what to include in prompts before question')
     parser.add_argument('--continue-partial', action='store_true', help='continue a previously started experiment (with the same output path)')
     parser.add_argument('--cpu-only', action='store_true', help='do not use GPU')
+    parser.add_argument('--qa-mode', choices=['mc', 'gen'], required=True)
 
     args = parser.parse_args()
     questions_path = Path(args.questions_path)
     output_path = Path(args.output_path)
 
-    main(args.model, questions_path, output_path, args.prompt_style, args.continue_partial, args.cpu_only)
+    if args.qa_mode == 'mc':
+        run_mc(args.model, questions_path, output_path, args.prompt_style, args.continue_partial, args.cpu_only)
+    elif args.qa_mode == 'gen':
+        run_gen(args.model, questions_path, output_path, args.prompt_style, args.continue_partial, args.cpu_only)
